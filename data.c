b28a91ed77f1db464afc873bc451c901e063f9ad
ffdy <scaryffy@qq.com>
2022-12-20 21:55:34 +0800

    data部分调整，使用宏替换数字

--- data.c
+++ data.c
@@ -1,18 +1,7 @@
 #include "data.h"
-// 状态矩阵，用于渲染图形
-// 内存状态，0空闲，1apple生产占用，2apple等待消费
-// 3orange生产占用，4orange等待消费
+
 int mem_state[N] = {0};
-// // [x][0]apple生产者，[x][1]orange生产者
-// int pStatMatrix[N][2] = {0};
-// // [x][0]apple消费者，[x][1]orange消费者
-// int cStatMatrix[N][2] = {0};
-// [x][0]apple producer [x][1]orange producer
-// 0空闲，1阻塞，2生产中
-// [x][2]apple consumer [x][3]orange consumer
-// 0空闲，1阻塞，2消费中
 int producer_consumer_state[N][4] = {0};
-// 生产者消费者工作颜色对应
 
 int work_time[N][4] = {0};
 int free_time[N][4] = {0};
--- data.h
+++ data.h
@@ -2,17 +2,28 @@
 #define N 20
 
 // 状态矩阵，用于渲染图形
-// 内存状态，0空闲，1apple生产占用，2apple等待消费, 5apple消费占用
-// 3orange生产占用，4orange等待消费，6orange消费占用
+#define MEM_FREE            0 // 空闲
+#define MEM_APPLE_PRODUCE   1 // apple生产占用
+#define MEM_APPLE_WAITING   2 // apple等待消费
+#define MEM_APPLE_CONSUME   5 // apple消费占用
+#define MEM_ORANGE_PRODUCE  3 // orange生产占用
+#define MEM_ORANGE_WAITING  4 // orange等待消费
+#define MEM_ORANGE_CONSUME  6 // orange消费占用
+// 内存状态
 extern int mem_state[N];
-// // [x][0]apple生产者，[x][1]orange生产者
-// int pStatMatrix[N][2] = {0};
-// // [x][0]apple消费者，[x][1]orange消费者
-// int cStatMatrix[N][2] = {0};
-// [x][0]apple producer [x][1]orange producer
-// 0空闲，1阻塞，2生产中
-// [x][2]apple consumer [x][3]orange consumer
-// 0空闲，1阻塞，2消费中
+
+#define APPLE_PRODUCER  0
+#define ORANGE_PRODUCER 1
+#define APPLE_CONSUMER  2
+#define ORANGE_CONSUMER 3
+#define MEMORY          4
+
+#define FREE      0 // 空闲
+#define WAITING   1 // 阻塞
+#define PRODUCING 2 // 生产中
+#define CONSUMING 2 // 消费中
+// [x][APPLE_PRODUCER]apple producer [x][ORANGE_PRODUCER]orange producer
+// [x][APPLE_CONSUMER]apple consumer [x][ORANGE_CONSUMER]orange consumer
 extern int producer_consumer_state[N][4];
 
 // 生产消费者工作和空闲时间
