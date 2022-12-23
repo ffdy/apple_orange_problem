c3de59e9378df84a4c3d02ab61d92d8cd6cb8028
ffdy <scaryffy@qq.com>
2022-12-23 11:24:04 +0800

    调整文件结构；信号量bug修复；view着色器程序链接bug修复；proc添加打印控制宏

--- proc.h
+++
@@ -1,7 +0,0 @@
-void *apple_producer(void *arg);
-void *orange_producer(void *arg);
-void *apple_consumer(void *arg);
-void *orange_consumer(void *arg);
-
-void proc_start();
-void proc_done();
--- view.h
+++
@@ -1,3 +0,0 @@
-void *view(void *arg);  // view线程工作
-void view_start();      // 创建view线程并运行
-void view_done();       // 通知view线程退出
